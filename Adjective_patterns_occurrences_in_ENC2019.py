925d2c18d8b4a9f99fade52f1a0685f784c6a1aa
ahtilohk <48084510+ahtilohk@users.noreply.github.com>
2022-04-19 12:19:15 +0300

    Update Adjective_patterns_occurrences_in_ENC2019.py

--- Adjective_patterns_occurrences_in_ENC2019.py
+++ Adjective_patterns_occurrences_in_ENC2019.py
@@ -1,4 +1,7 @@
 """ 
+
+*** Adjective_patterns_occurrences_in_ENC2019_with_estnltk_corpus_processing_module ***
+
 The purpose of the program is to go through all sub-corpora of the ENC2019 text corpus 
 and find all occurrences of test words in the given test patterns.
 
